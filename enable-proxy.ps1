61c76743ded9ea1c5786903c5d9f4329f1e02a50
Feodor Fitsner <feodor@appveyor.com>
2016-04-12 14:24:22 -0700

    universal proxy script - version 1

--- enable-proxy.ps1
+++ enable-proxy.ps1
@@ -1,6 +1,38 @@
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d 10.240.0.29:8888 /f
+# Proxy addresses
+$proxies = @{
+    "146.148.85.29" = "10.240.0.88"   # a
+    "104.197.145.181" = "10.240.0.2"  # b
+    "104.197.110.30" = "10.240.0.21"  # c
+    "104.197.65.101" = "10.240.0.74"  # f
+}
 
+# Enable SSL3
+$origProtocol = [Net.ServicePointManager]::SecurityProtocol
+[Net.ServicePointManager]::SecurityProtocol = 'Tls12'
+
+# get external IP
+$wc = (New-Object Net.WebClient)
+$wc.Headers.Add("User-Agent", "AppVeyor")
+$ip = $wc.DownloadString('https://canhazip.com/').Trim()
+
+# restore original protocol
+[Net.ServicePointManager]::SecurityProtocol = $origProtocol
+
+$proxy_ip = $proxies[$ip]
+$proxy_port = "8888"
+
+if(-not $proxy_ip) {
+    # IP address not found
+    # no proxy for the given address
+    return
+}
+
+Write-Host "Enabling AppVeyor HTTP proxy..."
+
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d "$proxy_ip`:$proxy_port" /f
+
+# notifying wininet
 $source=@"
 
 [DllImport("wininet.dll")]
@@ -10,7 +42,6 @@ public static extern bool InternetSetOption(int hInternet, int dwOption, int lpB
 "@
 
 #Create type from source
-
 $wininet = Add-Type -memberDefinition $source -passthru -name InternetSettings
 
 #INTERNET_OPTION_PROXY_SETTINGS_CHANGED
@@ -19,24 +50,6 @@ $wininet::InternetSetOption([IntPtr]::Zero, 95, [IntPtr]::Zero, 0)|out-null
 #INTERNET_OPTION_REFRESH
 $wininet::InternetSetOption([IntPtr]::Zero, 37, [IntPtr]::Zero, 0)|out-null
 
-[Net.ServicePointManager]::SecurityProtocol = 'Tls12'
-Invoke-WebRequest https://canhazip.com
-
-$mavenConfig = '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
-                  http://maven.apache.org/xsd/settings-1.0.0.xsd">
-
-  <proxies>
-    <proxy>
-      <active>true</active>
-      <protocol>http</protocol>
-      <host>10.240.0.29</host>
-      <port>8888</port>
-    </proxy>
-  </proxies>
-
-</settings>'
-
-New-Item "$env:USERPROFILE\.m2" -ItemType Directory -Force
-Set-Content "$env:USERPROFILE\.m2\settings.xml" -Value $mavenConfig
+# save proxy details in environment variables
+$env:APPVEYOR_HTTP_PROXY_IP = $proxy_ip
+$env:APPVEYOR_HTTP_PROXY_PORT = $proxy_port
