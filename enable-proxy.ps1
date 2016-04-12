9b7599b30ac797037ae960ca89a6cf7792bc4ebe
Feodor Fitsner <feodor@appveyor.com>
2016-04-12 14:48:27 -0700

    out-null reg settings

--- enable-proxy.ps1
+++ enable-proxy.ps1
@@ -30,8 +30,8 @@ if(-not $proxy_ip) {
     return
 }
 
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d "$proxy_ip`:$proxy_port" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f | out-null
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d "$proxy_ip`:$proxy_port" /f | out-null
 
 # notifying wininet
 $source=@"
